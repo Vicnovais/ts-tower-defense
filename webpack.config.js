4e153a7a7764d002a374a8de80a02d75c14a6096
Victor Novais <vicnovais@gmail.com>
2020-04-14 22:06:44 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    mode: "production",
+    devtool: "source-map",
+
+    resolve: {
+        extensions: [".ts"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    }
+};
