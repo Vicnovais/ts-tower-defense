606afdf283eff089444665535d254e2bbb170e56
Victor Novais <vicnovais@gmail.com>
2020-04-25 21:58:26 -0300

    Path builder

--- src/map.ts
+++ src/map.ts
@@ -2,16 +2,15 @@ import LevelSchema from "./map.levels";
 import Tile from "./tile";
 import Colors from "./colors";
 import $ from "jquery";
+import _ from "underscore";
 import Waypoint from "./waypoint";
 import Engine from "./engine";
 
 class Map {
-    private readonly level: number;
     private readonly levelSchema: LevelSchema;
     private wayPoints: Waypoint[];
 
     constructor(level: number) {
-        this.level = level;
         this.levelSchema = new LevelSchema(level);
         this.wayPoints = [];
     }
@@ -64,6 +63,75 @@ class Map {
 
     appendMonster($elem: JQuery<HTMLElement>) {
         $("#map").append($elem);
+    }
+
+    getPath(schema: Tile[][]) {
+        let findNeighbors = (position: number[]) => {
+            let i = position[0],
+                j = position[1],
+                neighbors = [
+                    [--i, j],
+                    [i, ++j],
+                    [i, ++j],
+                    [i, --j],
+                    [--i, --j],
+                    [--i, ++j],
+                    [++i, --j],
+                    [++i, ++j]
+                ];
+
+            neighbors = neighbors.filter(t => {
+                let i = t[0],
+                    j = t[1];
+
+                return i >= 0 && j >= 0 && schema[i] && schema[i][j];
+            });
+
+            return neighbors;
+        };
+
+        let findTile = (tile: Tile) => {
+            for (let i = 0; i < schema.length; i++) {
+                for (let j = 0; j < schema[i].length; j++) {
+                    if (schema[i][j] === tile) return [i, j];
+                }
+            }
+
+            return null;
+        };
+
+        let findNextPath = (position: number[]): number[] => {
+            let i = position[0],
+                j = position[1],
+                neighbors = findNeighbors(position),
+                paths = neighbors.filter(t => {
+                    let i = t[0],
+                        j = t[1];
+
+                    return schema[i][j] === Tile.PATH ||
+                           schema[i][j] === Tile.WAYPOINT ||
+                           schema[i][j] === Tile.EXIT;
+                });
+
+            if (paths.length > 1) throw new Error(`Multiple paths found for position: (${ i }, ${ j })`);
+            return _.flatten(paths);
+        };
+
+        let entranceCoord = findTile(Tile.ENTER);
+        if (!entranceCoord) throw new Error("Map entrance not found.");
+
+        let exitCoord = findTile(Tile.EXIT);
+        if (!exitCoord) throw new Error("Map exit not found.");
+
+        let path: number[][] = [];
+
+        do {
+            let currentTile = entranceCoord,
+                nextPath = findNextPath(currentTile);
+
+            path.push(nextPath);
+            currentTile = nextPath;
+        } while (true);
     }
 }
 
